81e7bfab8eae5b74d5ca51140dab2167674b4b89
Lee Dong Bok <ehdqhr3408@naver.com>
2020-06-24 20:28:48 +0900

    java integer test

---
+++ Code02.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Code02 {
+
+    public static void main(String[] args){
+        int number = 123;
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Please enter an integer: ");
+        int input = keyboard.nextInt();
+
+        if(input==number){
+            System.out.print("Numbers match! :-)");
+        }
+        else{
+            System.out.print("Numbers do not match! :-(");
+        }
+
+        keyboard.close();
+    }
+}
