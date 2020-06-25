cd530a71636e481bb4ac4fe9b414c20b4ca7254d
Lee Dong Bok <ehdqhr3408@naver.com>
2020-06-25 22:22:06 +0900

    문자열 연습 코드 1

---
+++ Code03.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Code03 {
+    public static void main(String[] args){
+        String str = "Hello";
+        String input = null;
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Please type a string: ");
+        //.next() 는 str = Hello World 여도 공백 앞자리 까지만 인식해서 Hello 만 받아들인다.
+        input = keyboard.next();
+        //문자열은 str == input 으로 비교할 수 없다.
+        if(str.equals(input)){
+            System.out.println("strings match! :-)");
+        }
+        else{
+            System.out.println("strings do not match! :-(");
+        }
+        keyboard.close();
+
+    }
+}
