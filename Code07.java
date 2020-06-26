d963392e5c08909020c2ffaeb3ae121bcf1ebcb5
Lee Dong Bok <ehdqhr3408@naver.com>
2020-06-26 23:55:12 +0900

    배열 활용 1

--- Code07.java
+++ Code07.java
@@ -3,6 +3,11 @@ public class Code07 {
 
         int[] grades;
 
+        // 알면 좋은 것!
+        // 배열의 크기는 c언어의 경우 상수만 지원한다. 하지만 자바는 변수도 지원함.
+        // ex) Scanner kb = new Scanner(System.in);
+        // ex) int n = kb.nextInt();
+        // ex) int [] array = new int [n];
         grades = new int[5];
 
         grades[0] = 100;
