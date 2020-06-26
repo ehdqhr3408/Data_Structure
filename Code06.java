4e7d9431c2b41593c9fb66ae686c0af897886299
Lee Dong Bok <ehdqhr3408@naver.com>
2020-06-26 23:46:50 +0900

    배열 연습 2

--- Code06.java
+++ Code06.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class Code06 {
+    public static void main(String[] args) {
+
+        int[] grades;
+
+        grades = new int[5]; // 원래 c 언어 같은 경우 #define Max 5
+
+        grades[0] = 100;
+        grades[1] = 76;
+        grades[2] = 92;
+        grades[3] = 95;
+        grades[4] = 14;
+
+        // 배열은 자신의 길이를 알고있어 length를 쓴다.
+        // c언어 같은 경우 grades.length 대신 i < Max
+        for (int i = 0; i < grades.length; i++) {
+            System.out.println("Grade " + (i + 1) + ": " + grades[i]);
+        }
+
+    }
 }
