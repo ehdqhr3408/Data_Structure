1bd054ad860c6453857d06deca8a27afcc91e117
Lee Dong Bok <ehdqhr3408@naver.com>
2020-06-30 05:59:36 +0900

    Ordered list insert algorithm

--- Code15.java
+++ Code15.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+/*
+사용자로부터 n개의 정수를 입력받는다. 정수가 하나씩 입력될 때 마다
+현재까지 입력된 정수들을 오름차순으로 정렬하여 출력하라.
+*/
+
+import java.util.Scanner;
 
 public class Code15 {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] data = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int tmp = kb.nextInt();
+            int j = i - 1;
+            while (j >= 0 && data[j] > tmp) {
+                data[j + 1] = data[j];
+                j--;
+            }
+            data[j + 1] = tmp;
+
+            for (int k = 0; k <= i; k++)
+                System.out.print(data[k] + " ");
+            System.out.println();
+        }
+        kb.close();
+    }
 }
