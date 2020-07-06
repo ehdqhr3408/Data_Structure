f0ff95ece6cf4f498fe7f2d095460c95fa9d3a33
Lee Dong Bok <ehdqhr3408@naver.com>
2020-07-06 12:51:07 +0900

    소수판단 함수, 1이 소수로 판단되는 이유찾기

--- Section02/Code17.java
+++ Section02/Code17.java
@@ -1,3 +1,6 @@
+/*
+함수를 사용해서 소수판단하기
+ */
 package Section02;
 
 public class Code17 {
@@ -8,6 +11,7 @@ public class Code17 {
         }
     }
     public static boolean isPrime(int k){
+        //k<2조건을 안달면 1도 소수로 출력된다
         if(k<2)
             return false;
         for(int i=2;i*i<=k;i++)
