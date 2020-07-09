0a8d21e51c74d310ca764fbf73f19dfb72a3b175
Lee Dong Bok <ehdqhr3408@naver.com>
2020-07-09 14:32:44 +0900

    태스크 03 완료

--- Section01/Task04.java
+++ Section01/Task04.java
@@ -7,5 +7,18 @@ ex) n=4일때
 1 2 3 4
 */
 
+import java.util.Scanner;
+
 public class Task04 {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        kb.close();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(j + 1 + " ");
+            }
+            System.out.println();
+        }
+    }
 }
