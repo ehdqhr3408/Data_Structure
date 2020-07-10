a0a6b541574c1250dd3e7842d82c7e36d60d4103
Lee Dong Bok <ehdqhr3408@naver.com>
2020-07-10 23:37:03 +0900

    태스크 05 진행중

--- Section01/Task05.java
+++ Section01/Task05.java
@@ -3,5 +3,15 @@ package Section01;/*
 출력하는 프로그램을 작성하라. 입력형식은 3번과 동일하다.
 */
 
+import java.util.Scanner;
+
 public class Task05 {
+    public static void main(String[] args){
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int data [] = new int [n];
+
+        for(int i=0;i<n;i++)
+            data[i] = kb.nextInt();
+    }
 }
