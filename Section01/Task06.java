45859263853404629736e403636a10c05953e942
Lee Dong Bok <ehdqhr3408@naver.com>
2020-07-12 21:25:13 +0900

    태스크 06 임시 완성 - 후에 다시 해볼 것

--- Section01/Task06.java
+++ Section01/Task06.java
@@ -3,5 +3,20 @@ package Section01;/*
 찾아서 출력하는 프로그램을 작성하라. 예를들어 n=12이면 출력은 2의4승=16이다
 */
 
+import java.util.Scanner;
+
 public class Task06 {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int num = 2;
+        int n = kb.nextInt();
+        kb.close();
+        while (true) {
+            if (num <= n)
+                num *= 2;
+            else
+                break;
+        }
+        System.out.println(num);
+    }
 }
