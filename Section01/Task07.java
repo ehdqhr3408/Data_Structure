d0c77a799115e452c569dc3bee3e023e47611a07
Lee Dong Bok <ehdqhr3408@naver.com>
2020-07-13 20:48:35 +0900

    태스크 07 미완성

--- Section01/Task07.java
+++ Section01/Task07.java
@@ -4,5 +4,29 @@ package Section01;/*
 n=5이고 정수들이 8, 1, 6, 9, 6이라면 이 중 서로 다르면서 차이가 최소인 두 수는 8과 9이다.
 */
 
+import java.util.Scanner;
+
 public class Task07 {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+
+        int data[] = new int[n];
+        for (int i = 0; i < n; i++)
+            data[i] = kb.nextInt();
+        kb.close();
+
+        int a = 0, b = 0;
+        int min = Math.abs(data[0] - data[1]);
+        for (int j = 0; j < n; j++) {
+            for (int k = 0; k < n; k++) {
+                if (data[j] != data[k] && Math.abs(data[j] - data[k]) < min) {
+                    min = Math.abs(data[j] - data[k]);
+                    a = j;
+                    b = k;
+                }
+            }
+        }
+        System.out.println(a + " " + b);
+    }
 }
